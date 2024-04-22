cdca8f354557200f9cf72567c33ad6be97e47041
khaled sayed <khaledsayed273@gmail.com>
2024-04-22 04:43:10 +0200

    first commit

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -28,12 +28,11 @@ async function services() {
 
                     <div className='mt-10'>
                         {services.status && (
-                            
                                 services.data.map((item) => (
                                     <div key={item.id} className='bg-[#E7F2FF] mb-10 grid md:grid-cols-2 gap-10 p-7 rounded-2xl'>
                                         <div className='flex flex-col justify-center items-start'>
                                             <h1 style={{lineHeight: "1.15"}} className='bg-gradient-to-r from-[#0061FF] to-[#47b4c0] text-transparent bg-clip-text font-bold text-3xl md:text-4xl lg:text-5xl'>{item.name}</h1>
-                                            <p className='text-[#01579B] mt-4 font-serif font-semibold'>{item.short_descrition}</p>
+                                            <p className='text-[#01579B] mt-4 font-serif font-semibold'>{item.short_description}</p>
                                             <Link href={`/services/${item.slug}`} className='bg-gradient-to-r from-[#0061FF] to-[#3c939c] mt-7 mb-5   rounded-full py-2 px-7 inline-flex justify-between items-center'>
                                                 <svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M1.5 7.93536L15.2232 7.79851M10.1182 1.41132L16.6412 7.9343L9.98677 14.5887" stroke="white" strokeWidth="2" strokeLinecap="square" strokeLinejoin="round" />
