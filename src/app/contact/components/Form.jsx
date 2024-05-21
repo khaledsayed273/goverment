01baf2e5a0ca8da54490d2a5f2b9d95aa3105c6e
khaled sayed <khaledsayed273@gmail.com>
2024-05-22 00:05:04 +0300

    first push

--- src/app/contact/components/Form.jsx
+++ src/app/contact/components/Form.jsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import { toast } from 'sonner'
+import { NextResponse } from 'next/server'
 
 function Form({ url }) {
 
@@ -40,6 +41,8 @@ function Form({ url }) {
                 setEmail("")
                 setPhone("")
                 setMessage("")
+            return NextResponse.json(res)
+
             }else{
                 toast.error('حدثت مشكلة برجاء المحاولة مرة اخري')
             }
