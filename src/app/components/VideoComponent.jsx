ae2494595bda7fd24712e03613d4ca84e8f7f8a3
khaledsayed273 <khaledsayed273@gmail.com>
2024-06-13 04:57:40 +0300

    first v

--- src/app/components/VideoComponent.jsx
+++ src/app/components/VideoComponent.jsx
@@ -5,7 +5,6 @@ function VideoComponent({ src }) {
         <video
             height={"100%"}
             width={"100%"}
-            preload="none"
             controls>
             <source src={src} type="video/mp4" />
         </video>
